b171e3d654f24ad145f5071ae5b904a23f0e32d2
Manish Kumar <65070286+manishkumar186@users.noreply.github.com>
2023-06-10 19:44:55 +0530

    Add files via upload

---
+++ Service-register/src/test/java/com/example/Serviceregister/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
